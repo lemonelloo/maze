ab4b0582be0b8f536f37e3affabce85aadd294cb
lemonelloo <129597691+lemonelloo@users.noreply.github.com>
2023-04-01 15:19:36 +0300

    Add files via upload

---
+++ maze.py
@@ -0,0 +1,121 @@
+from pygame import *
+
+init()
+mixer.init()
+
+WIDTH = 700
+HEIGHT = 500
+FPS = 60
+
+font.init()
+font = font.Font(None, 70)
+win = font.render('YOU WIN!', True, (255, 215, 0))
+lose = font.render('YOU LOSE!', True, (180, 0, 0))
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys_pressed[K_s] and self.rect.y < 395:
+            self.rect.y += self.speed
+        if keys_pressed[K_a] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys_pressed[K_d] and self.rect.x < 595:
+            self.rect.x += self.speed
+
+class Enemy(GameSprite):
+    direction = 'left'
+    def update(self):
+        if self.rect.x <= 470:
+            self.direction = 'right'
+        if self.rect.x >= 640:
+            self.direction = 'left'
+        if self.direction == 'left':
+            self.rect.x -= self.speed
+        if self.direction == 'right':
+            self.rect.x += self.speed
+
+class Wall(sprite.Sprite):
+    def __init__(self, color_1, color_2, color_3, wall_x, wall_y, wall_width, wall_height):
+        super().__init__()
+        self.color_1 = color_1
+        self.color_2 = color_2
+        self.color_3 = color_3
+        self.width = wall_width
+        self.height = wall_height
+        self.image = Surface((self.width, self.height))
+        self.image.fill((color_1, color_2, color_3))
+        self.rect = self.image.get_rect()
+        self.rect.x = wall_x
+        self.rect.y = wall_y
+    def draw_wall(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+window = display.set_mode((WIDTH, HEIGHT))
+display.set_caption('MAZE')
+bg = transform.scale(image.load("background.jpg"), (WIDTH, HEIGHT))
+clock = time.Clock()
+init()
+
+player = Player('hero.png', 5, HEIGHT-80, 5)
+monster= GameSprite('cyborg.png', WIDTH-80, 280, 4)
+final = GameSprite('treasure.png', WIDTH-120, HEIGHT-80, 0)
+
+w1 = Wall(154, 205, 50, 100, 20, 450, 10)
+w2 = Wall(154, 205, 50, 100, 480, 350, 10)
+w3 = Wall(154, 205, 50, 100, 20, 10, 380)
+
+
+mixer.music.load("jungles.ogg")
+mixer.music.play()
+
+money = mixer.Sound('money.ogg')
+kick = mixer.Sound('kick.ogg')
+
+game = True
+finish = False
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    if finish != True:
+        window.blit(bg, (0, 0))
+        player.update()
+        player.reset()
+
+        monster.update()
+        monster.reset()
+
+        final.reset()
+
+        w1.draw_wall()
+        w2.draw_wall()
+        w3.draw_wall()
+
+        if sprite.collide_rect(player, monster) or sprite.collide_rect(player, w1) or sprite.collide_rect(player, w2) or sprite.collide_rect(player, w3):
+            finish = True
+            window.blit(lose, (200, 200))
+            kick.play()
+
+        if sprite.collide_rect(player, final):
+            finish = True
+            window.blit(win, (200, 200))
+            money.play()
+
+    display.update()
+    clock.tick(FPS)
